49b5ae238167c16cef65f832d5228542b355c2c2
Colin Cooke <colin11cooke@gmail.com>
2015-02-22 02:02:15 -0500

    twitterand reddit

---
+++ reddit.py
@@ -0,0 +1,67 @@
+# get_joke needs nothing
+# get_til needs nothing
+# get_quote needs nothing
+
+import praw
+
+def get_joke():
+    try:
+        r = praw.Reddit(user_agent='example')
+        r = r.get_random_submission('jokes')
+        title =  r.title
+        text = r.selftext
+        if((len(text) + 3 + len(title) )> 160):
+            text = text.encode('ascii','ignore')
+            title = title.encode('ascii','ignore')
+            return "Your a joke"
+
+        else:
+            return title + ( (" - " + text) if len(text) !=0 else "")
+    except:
+        return "www.zombo.com"
+def get_til():
+    try:
+        r = praw.Reddit(user_agent='example')
+        r = r.get_random_submission('TIL')
+        title =  r.title
+        if((len(title))> 160):
+            return "TIL you're stupid"
+        else:
+            title = title.encode('ascii','ignore')
+            return title
+    except:
+        return "www.zombo.com"
+def get_quote():
+    try:
+        r = praw.Reddit(user_agent='example')
+        r = r.get_random_submission('quotes')
+        title =  r.title
+        if((len(title))> 160):
+            return "Fuck off you maggot - Ghandi"
+        else:
+            title = title.encode('ascii','ignore')
+            return title
+    except:
+        return "www.zombo.com"
+def do_joke(input):
+    if "joke" in input:
+        return "valid"
+    else:
+        return "invalid"
+
+def do_til():
+    if "fact" in input:
+        return "valid"
+    else:
+        return "invalid"
+
+
+def do_quote():
+    if "quote" in input:
+        return "valid"
+    else:
+        return "invalid"
+
+print get_quote()
+print get_til()
+print get_joke()
---
+++ twitter.py
@@ -0,0 +1,62 @@
+# get_hashtag needs the text
+# get_handle needs the text
+
+from twython import Twython
+
+def get_hashtag(tag):
+    try:
+        APP_KEY = 'MDszGMPdCSC6ujOVB86YXwwMX'
+        ACCESS_TOKEN = "AAAAAAAAAAAAAAAAAAAAAMNpeQAAAAAAzW0NtAlnkRbxa%2FvGJc2iKxoz8oM%3DVjIzeLDaWXBPybTv8mKRlXgDoQWbQW56ZbM4xcxk5yPRlBXsQs"
+
+
+        twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+
+        if(tag[0] != '#'):
+            tag = '#' + tag
+
+        results = twitter.search(q=tag, result_type='recent', lang="en",count=1)
+
+        results = results[results.keys()[1]][0]
+        result = results[results.keys()[2]]
+        result = result.encode('ascii','ignore')
+        return result
+    except:
+        return "www.zombo.com"
+
+
+def get_handle(handle):
+        try:
+            APP_KEY = 'MDszGMPdCSC6ujOVB86YXwwMX'
+            ACCESS_TOKEN = "AAAAAAAAAAAAAAAAAAAAAMNpeQAAAAAAzW0NtAlnkRbxa%2FvGJc2iKxoz8oM%3DVjIzeLDaWXBPybTv8mKRlXgDoQWbQW56ZbM4xcxk5yPRlBXsQs"
+
+            twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+
+            if(handle[0] != '@'):
+                handle = '@' + handle
+
+            results = twitter.search(q=handle, result_type='recent', lang="en",count=1)
+
+            results = results[results.keys()[1]][0]
+            result = results[results.keys()[2]]
+            result = result.encode('ascii','ignore')
+            return result
+        except:
+            return "www.zombo.com"
+
+
+def do_handle(input):
+    if "@" in input:
+        return "valid"
+    else:
+        return "invalid"
+
+def do_hashtag(input):
+    if "#" in input:
+        return "valid"
+    else:
+        return "invalid"
+
+
+
+print twitter_hashtag("yolo")
+print twitter_handle("yolo")
