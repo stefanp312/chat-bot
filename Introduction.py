6d3540f7b8eb4a9087ed80295002b7ef02ccfa1a
Stefan Pleava <stefan@osellus-mobile.com>
2015-02-22 11:40:56 -0500

    commit with intro text and things

--- Introduction.py
+++ Introduction.py
@@ -1,17 +1,46 @@
-def helloworld(input):
-	input = input.lower()
-	arr = input.split()
-	key_counter = 0
 
-	for i in range (0,len(arr)):
-		if arr[i] == "wat"or arr[i] == "what"or arr[i] == "name"or arr[i] == "name?" or arr[i] == "what's" or arr[i] == "wats" or arr[i] == "?" or arr[i] == "your" or arr[i] == "ur" or arr[i] == "whats":
-			key_counter=key_counter+1
-	if key_counter>2:
-		# print "My name is Jonas!\nhttp://youtu.be/yk0_i8u_bgs"
-		return "My name is Jonas!\nhttp://youtu.be/yk0_i8u_bgs"
+# name_term_list=["your name","ur name","youre name"]
+# salute_term_list=["hi","hello","how are you","good morning","sup","good evening","goodnight","goodnight"]
+from random import randint
+
+def salute(keyword):
+	try:
+		bye_term_list = ["good evening", "goodnight", "goodnight"]
+		if keyword in bye_term_list:
+			return "Goodbye!"
+		else:
+			i = randint(1, 9)
+			if i == 1:
+				return "Top of the morning to ya!"
+			elif i == 2:
+				return "Hey pal!"
+			elif i == 3:
+				return "What's cookin' good lookin'?"
+			elif i==4:
+				return "HI! I AM EXCITED TO TALK TO YOU"
+			elif i==5:
+				return "G'day mate"
+			elif i==6:
+				return "Sup dawg \n (\"up dawg\"...\"what's up dawg?\")"
+			elif i==7:
+				return "Well hello there :)"
+			elif i==8:
+				return "Hey, baby, won't you look my way?\nI can be your new addiction\nHey, baby, what you gotta say?\nAll you're giving me is fiction"
+			else:
+				return "Well aren't you a social butterfly, approaching me like that!"
+	except:
+		return "Invalid"
 		
-	else:
+def give_name():
+	try:
+		return "My name is Jonas!\nhttp://youtu.be/VoAnLHSLHGQ\n#weezer"
+	except:
 		return "Invalid"
 
+def what_do():
+	try:
+		return "I am chatbot that can search twitter, wikipedia, reddit, with natural language processing."
+	except:
+		return "Invalid"
 
-	
+print salute("Hi")
--- testNat.py
+++ testNat.py
@@ -2,7 +2,7 @@ from alchemyapi import AlchemyAPI
 import reddit
 import twitter
 import wiki
-import json
+import Introduction
 
 def process_text(query=""):
     alchemyapi = AlchemyAPI()
@@ -11,6 +11,10 @@ def process_text(query=""):
     jokes_term_list = ["jokes", "joke", "tell me a joke", "funny joke", "humor me", "humor", "make me laugh", "laugh", "pun", "one liner", "smartass"]
     search_term_list = ["search", "where", "when was", "when did", "who is", "who was", "when was", "tell me about", "what is", "tell", "how does", "what makes", "question", "wikipedia", "how to", "find", "wat is", "wat makes", "wut is"]
     twitter_term_list = ["tweet", "twitter", "#", "hashtag", "funny tweet", "updates about"]
+    name_term_list = ["your name", "ur name", "youre name", "wat are you", "what are you"]
+    salute_term_list = ["hi", "hello", "how are you", "good morning", "sup", "good evening", "goodnight", "goodnight"]
+    til_term_list = ["fact", "til", "learn", "cool facts"]
+
     if response2['status'] == 'OK':
 
         print "Sentiment: ", response2["docSentiment"]["type"]
@@ -37,6 +41,18 @@ def process_text(query=""):
     else:
         print('Error in combined call: ', response['statusInfo'])
 
+    for term in name_term_list:
+        if term in query.lower():
+            print "name stuff"
+            return Introduction.give_name()
+    for term in salute_term_list:
+        if term in query.lower():
+            print "hi stuff"
+            return Introduction.salute(term)
+    for term in til_term_list:
+        if term in query.lower():
+            print "til reddit"
+            return reddit.get_til()
     for term in jokes_term_list:
         if term in query.lower():
             print "jokes"
@@ -56,3 +72,9 @@ def process_text(query=""):
         print "I'm sorry you're sad. Here's a joke."
         return reddit.get_joke()
     return "Invalid"
+
+print process_text("Hi")
+print process_text("What is your name?")
+print process_text("Hello what are you?")
+print process_text("What do you do?")
+print process_text("fact")
