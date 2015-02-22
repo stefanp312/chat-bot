1be4fec1613a59b509e89abc399da8457edc6a43
Stefan Pleava <stefan@osellus-mobile.com>
2015-02-22 02:15:11 -0500

    lopl

--- run.py
+++ run.py
@@ -17,13 +17,13 @@ def main_reply():
     # create cmds variable from cookies
     cmds = session.get('cmds', [""])
     searchs = session.get('searchs', [["", 0]])
-    reply = searchwikipedia(recieved_message)
+    reply = wiki.searchwikipedia(recieved_message)
     # trim the length of the reply to one text
     if len(reply) > 160:
         reply = reply[0:159]
-    elif reply == "":
+    if reply == "":
         reply = "Error"
-        
+
     # get the response scheme from twilio and add reply as message body
     resp = twilio.twiml.Response()
     resp.message(reply)
