fd9f9f6dcdcefa0c8b124a5f404c04f1073c2d5a
Colin Cooke <colin11cooke@gmail.com>
2015-02-22 09:12:11 -0500

    showing twitter its a bitch

--- twitter.py
+++ twitter.py
@@ -2,33 +2,36 @@
 # get_handle needs the text
 
 from twython import Twython
+import json
 
 def get_hashtag(tag):
+    
     try:
-        print "##############################"
         APP_KEY = 'MDszGMPdCSC6ujOVB86YXwwMX'
         ACCESS_TOKEN = "AAAAAAAAAAAAAAAAAAAAAMNpeQAAAAAAzW0NtAlnkRbxa%2FvGJc2iKxoz8oM%3DVjIzeLDaWXBPybTv8mKRlXgDoQWbQW56ZbM4xcxk5yPRlBXsQs"
-        print "##############################"
+        print "#############################cxfsfgvvxgfd#"
         twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
         if(tag[0] != '#'):
             tag = '#' + tag
-        print "##############################"
+
         
         results = twitter.search(q=tag, result_type='recent', lang="en",count=1)
-        print results
-    
-        results = results[results.keys()[0]][0]
-        result = results[results.keys()[2]]
-        print result
-        result = result.encode('ascii','ignore')
-        return result
+
+
+        keys = results.keys()
+        key = ""
+        if "status" in keys[0]:
+            key = keys[0]
+        else:
+            key = keys[1]  
+        result = results[key]
+        text = result[0]['text']
+        return text.encode("utf-8")
     except:
         return "www.zombo.com"
 
-
 def get_handle(handle):
         try:
-            print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
             APP_KEY = 'MDszGMPdCSC6ujOVB86YXwwMX'
             ACCESS_TOKEN = "AAAAAAAAAAAAAAAAAAAAAMNpeQAAAAAAzW0NtAlnkRbxa%2FvGJc2iKxoz8oM%3DVjIzeLDaWXBPybTv8mKRlXgDoQWbQW56ZbM4xcxk5yPRlBXsQs"
 
@@ -39,10 +42,15 @@ def get_handle(handle):
 
             results = twitter.search(q=handle, result_type='recent', lang="en",count=1)
 
-            results = results[results.keys()[1]][0]
-            result = results[results.keys()[2]]
-            result = result.encode('ascii','ignore')
-            return result
+            keys = results.keys()
+            key = ""
+            if "status" in keys[0]:
+                key = keys[0]
+            else:
+                key = keys[1]  
+            result = results[key]
+            text = result[0]['text']
+            return text.encode("utf-8")
         except:
             return "www.zombo.com"
 
