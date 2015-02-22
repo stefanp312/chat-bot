2d4dac241d22f15a1a5c0db0878c29714d918753
Stefan Pleava <stefan@osellus-mobile.com>
2015-02-22 02:56:27 -0500

    revert code

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, session
 import twilio.twiml
-
+import wikipedia
 
 SECRET_KEY = 'donuts'
 app = Flask(__name__)
@@ -17,13 +17,29 @@ def main_reply():
     # create cmds variable from cookies
     cmds = session.get('cmds', [""])
     searchs = session.get('searchs', [["", 0]])
-    reply = "wiki.searchwikipedia(recieved_message)"
+
+    if "image" in recieved_message:
+        # send the wikipedia page iamge
+        print "not working"
+    elif "more" in recieved_message:
+        # query wikipedia with the recieved message and get next sentence
+        cmds.append("more")
+        last_search = searchs[len(searchs) - 1]
+        print last_search
+        last_character_seen = last_search[1]
+        print last_character_seen
+        reply = searchwikipedia(query=recieved_message, sentences=4)
+        next_search_index = reply.index(". ", beg=last_character_seen)
+        reply = reply[last_character_seen:next_search_index]
+        print reply
+    else:
+        # query wikipedia with the recieved message
+        reply = searchwikipedia(query=recieved_message)
+        searchs.append([recieved_message, len(reply)])
 
     # trim the length of the reply to one text
     if len(reply) > 160:
         reply = reply[0:159]
-    # if reply == "":
-    #  reply = "Error"
 
     # get the response scheme from twilio and add reply as message body
     resp = twilio.twiml.Response()
@@ -36,6 +52,15 @@ def main_reply():
     session['searchs'] = searchs
 
     return str(resp)
+
+def searchwikipedia(query, sentences=1):
+    summary = ""
+    try:
+        summary = wikipedia.summary(query, sentences=sentences)
+    except wikipedia.exceptions.DisambiguationError as e:
+        # + ", ".join(e.options)
+        summary = wikipedia.summary(e.options[1], sentences=sentences)
+    return summary
 
 if __name__ == "__main__":
     app.run(debug=True)
