e5d1a5b20f0b7d3de691af85522a051d3add3c69
Stefan Pleava <stefan@osellus-mobile.com>
2015-02-22 03:51:58 -0500

    updated and fixed lint

--- navigation.py
+++ navigation.py
@@ -4,8 +4,9 @@ import reddit.py
 
 def choose_script(bodyText=""):
     if doJoke(bodyText) == "Valid":
-		getJoke(bodyText)
-	if do_handle(bodyText) == "Valid":
-		get_handle(bodyText)
-	if do_hashtag(bodyText) == "Valid":
-		get_hashtag(bodyText)
+        return getJoke(bodyText)
+    if do_handle(bodyText) == "Valid":
+        return get_handle(bodyText)
+    if do_hashtag(bodyText) == "Valid":
+        return get_hashtag(bodyText)
+    return "Invalid"
